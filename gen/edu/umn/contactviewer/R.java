aec011800b74648d8b7ab6c63dbaa8ba3ecfb73e
xin <xin@192.168.1.5>
2013-02-26 20:33:42 -0600

    before push upstream for the first time

--- gen/edu/umn/contactviewer/R.java
+++ gen/edu/umn/contactviewer/R.java
@@ -16,30 +16,67 @@ public final class R {
         public static final int border=0x7f040004;
         public static final int foreground_start=0x7f040002;
         public static final int foreground_stop=0x7f040003;
-        public static final int my_color=0x7f040006;
+        public static final int my_color=0x7f040008;
         public static final int tool_text_color=0x7f040005;
+        public static final int toolbar_start=0x7f040006;
+        public static final int toolbar_stop=0x7f040007;
     }
     public static final class drawable {
         public static final int button_background=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int item_background=0x7f020002;
-        public static final int logo=0x7f020003;
-        public static final int toolbar_background=0x7f020004;
-        public static final int window_background=0x7f020005;
+        public static final int detail_background=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int item_background=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int toolbar_background=0x7f020005;
+        public static final int window_background=0x7f020006;
     }
     public static final class id {
-        public static final int item_icon=0x7f070002;
-        public static final int item_name=0x7f070000;
-        public static final int item_phone=0x7f070003;
-        public static final int item_title=0x7f070001;
-        public static final int toolbar=0x7f070004;
-        public static final int toolbar_right_button=0x7f070006;
-        public static final int toolbar_title=0x7f070005;
+        public static final int TableLayout1=0x7f070000;
+        public static final int TableLayout2=0x7f070010;
+        public static final int detailsCaption_email=0x7f07000b;
+        public static final int detailsCaption_name=0x7f070002;
+        public static final int detailsCaption_phone=0x7f070005;
+        public static final int detailsCaption_title=0x7f070008;
+        public static final int detailsCaption_twitterId=0x7f07000e;
+        public static final int details_email=0x7f07000c;
+        public static final int details_name=0x7f070003;
+        public static final int details_phone=0x7f070006;
+        public static final int details_title=0x7f070009;
+        public static final int details_twitterId=0x7f07000f;
+        public static final int editCaption_email=0x7f07001b;
+        public static final int editCaption_name=0x7f070012;
+        public static final int editCaption_phone=0x7f070015;
+        public static final int editCaption_title=0x7f070018;
+        public static final int editCaption_twitterId=0x7f07001e;
+        public static final int edit_email=0x7f07001c;
+        public static final int edit_name=0x7f070013;
+        public static final int edit_phone=0x7f070016;
+        public static final int edit_title=0x7f070019;
+        public static final int edit_twitterId=0x7f07001f;
+        public static final int item_icon=0x7f070022;
+        public static final int item_name=0x7f070020;
+        public static final int item_phone=0x7f070023;
+        public static final int item_title=0x7f070021;
+        public static final int tableRow1=0x7f070001;
+        public static final int tableRow10=0x7f07001d;
+        public static final int tableRow2=0x7f070004;
+        public static final int tableRow3=0x7f070007;
+        public static final int tableRow4=0x7f07000a;
+        public static final int tableRow5=0x7f07000d;
+        public static final int tableRow6=0x7f070011;
+        public static final int tableRow7=0x7f070014;
+        public static final int tableRow8=0x7f070017;
+        public static final int tableRow9=0x7f07001a;
+        public static final int toolbar=0x7f070024;
+        public static final int toolbar_right_button=0x7f070026;
+        public static final int toolbar_title=0x7f070025;
     }
     public static final class layout {
-        public static final int list=0x7f030000;
-        public static final int list_item=0x7f030001;
-        public static final int toolbar=0x7f030002;
+        public static final int details=0x7f030000;
+        public static final int edit=0x7f030001;
+        public static final int list=0x7f030002;
+        public static final int list_item=0x7f030003;
+        public static final int toolbar=0x7f030004;
     }
     public static final class string {
         public static final int about=0x7f050003;
@@ -49,10 +86,11 @@ public final class R {
         public static final int hello_world=0x7f050000;
     }
     public static final class style {
-        public static final int MainButton=0x7f060001;
+        public static final int DtTheme=0x7f060001;
+        public static final int MainButton=0x7f060002;
         public static final int MnTheme=0x7f060000;
-        public static final int Toolbar=0x7f060002;
-        public static final int ToolbarButton=0x7f060003;
-        public static final int ToolbarTitle=0x7f060004;
+        public static final int Toolbar=0x7f060003;
+        public static final int ToolbarButton=0x7f060004;
+        public static final int ToolbarTitle=0x7f060005;
     }
 }
--- src/edu/umn/contactviewer/ContactListActivity.java
+++ src/edu/umn/contactviewer/ContactListActivity.java
@@ -1,21 +1,29 @@
 package edu.umn.contactviewer;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
-import android.widget.AdapterView.*;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+import edu.umn.contactviewer.data.ContactProvider;
+import edu.umn.contactviewer.data.ContactViewerDatabase;
 
 /** Displays a list of contacts.
  *
  */
 public class ContactListActivity extends ListActivity {
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,51 +40,17 @@ public class ContactListActivity extends ListActivity {
 			}
 		});
         
-        // make some contacts
-        ArrayList<Contact> contacts = new ArrayList<Contact>();
-        contacts.add(new Contact("Malcom Reynolds")
-    		.setEmail("mal@serenity.com")
-    		.setTitle("Captain")
-    		.setPhone("612-555-1234")
-    		.setTwitterId("malcomreynolds"));
-        contacts.add(new Contact("Zoe Washburne")
-			.setEmail("zoe@serenity.com")
-			.setTitle("First Mate")
-			.setPhone("612-555-5678")
-			.setTwitterId("zoewashburne"));
-        contacts.add(new Contact("Hoban Washburne")
-			.setEmail("wash@serenity.com")
-			.setTitle("Pilot")
-			.setPhone("612-555-9012")
-			.setTwitterId("wash"));
-        contacts.add(new Contact("Jayne Cobb")
-			.setEmail("jayne@serenity.com")
-			.setTitle("Muscle")
-			.setPhone("612-555-3456")
-			.setTwitterId("heroofcanton"));
-        contacts.add(new Contact("Kaylee Frye")
-			.setEmail("kaylee@serenity.com")
-			.setTitle("Engineer")
-			.setPhone("612-555-7890")
-			.setTwitterId("kaylee"));
-        contacts.add(new Contact("Simon Tam")
-			.setEmail("simon@serenity.com")
-			.setTitle("Doctor")
-			.setPhone("612-555-4321")
-			.setTwitterId("simontam"));
-        contacts.add(new Contact("River Tam")
-			.setEmail("river@serenity.com")
-			.setTitle("Doctor's Sister")
-			.setPhone("612-555-8765")
-			.setTwitterId("miranda"));
-        contacts.add(new Contact("Shepherd Book")
-			.setEmail("shepherd@serenity.com")
-			.setTitle("Shepherd")
-			.setPhone("612-555-2109")
-			.setTwitterId("shepherdbook"));
+		String[] projection = { ContactViewerDatabase.ID, ContactViewerDatabase.COL_NAME, ContactViewerDatabase.COL_PHONE, ContactViewerDatabase.COL_TITLE};
+	    String[] uiBindFrom = { ContactViewerDatabase.COL_NAME, ContactViewerDatabase.COL_PHONE, ContactViewerDatabase.COL_TITLE};
+	    int[] uiBindTo = { R.id.item_name, R.id.item_phone, R.id.item_title };
+	    
+	    Cursor contacts = managedQuery(
+	            ContactProvider.CONTENT_URI, projection, null, null, null);
+	    CursorAdapter adapter = new ContactAdapter(getApplicationContext(), R.layout.list_item, contacts,
+	            uiBindFrom, uiBindTo);
         
-        // initialize the list view
-        setListAdapter(new ContactAdapter(this, R.layout.list_item, contacts));
+	    // initialize the list view
+	    setListAdapter(adapter);
         ListView lv = getListView();
         lv.setTextFilterEnabled(true);
         
@@ -84,40 +58,53 @@ public class ContactListActivity extends ListActivity {
         lv.setOnItemClickListener(new OnItemClickListener() {
         	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				// When clicked, show a toast with the TextView text
-				Toast.makeText(getApplicationContext(), 
-					"Clicked: " + ((ContactAdapter)getListAdapter()).getItem(position).getName(),
-					Toast.LENGTH_SHORT).show();
+				//Toast.makeText(getApplicationContext(), 
+					//"Clicked: " + ((ContactAdapter)getListAdapter()).getItem(position).getName(),
+					//Toast.LENGTH_SHORT).show();
+        		
+        		String projection[] = { ContactViewerDatabase.COL_NAME,  ContactViewerDatabase.COL_PHONE, ContactViewerDatabase.COL_TITLE, ContactViewerDatabase.COL_EMAIL, ContactViewerDatabase.COL_TWITTERID};
+        	    Cursor contactCursor = getContentResolver().query(
+        	            Uri.withAppendedPath(ContactProvider.CONTENT_URI,
+        	                    String.valueOf(id)), projection, null, null, null);
+        	    if (contactCursor.moveToFirst()) {
+						Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
+						
+						String name = contactCursor.getString(0);
+						String email = contactCursor.getString(1);
+						String title = contactCursor.getString(2);
+						String phone = contactCursor.getString(3);
+						String twitterId = contactCursor.getString(4);
+						
+				    	intent.putExtra("name", name);
+				    	intent.putExtra("email", email);
+				    	intent.putExtra("title", title);
+				    	intent.putExtra("phone", phone);
+				    	intent.putExtra("twitterId", twitterId);
+				    	startActivity(intent);
+        	    	}
+        	    contactCursor.close();
 				}
 			}
         );
         
     }
     
-    
-
-
 	/* We need to provide a custom adapter in order to use a custom list item view.
 	 */
-	public class ContactAdapter extends ArrayAdapter<Contact> {
+	public class ContactAdapter extends SimpleCursorAdapter {
 	
-		public ContactAdapter(Context context, int textViewResourceId, List<Contact> objects) {
-			super(context, textViewResourceId, objects);
+		public ContactAdapter(Context context, int layout, Cursor c, String[] from, int[] to)
+		{
+			super(context, layout, c, from, to);
 		}
 	
 		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
+		public View newView(Context context, Cursor cursor, ViewGroup parent) {
 			LayoutInflater inflater = getLayoutInflater();
 			View item = inflater.inflate(R.layout.list_item, parent, false);
-			
-			Contact contact = getItem(position);
-			((TextView)item.findViewById(R.id.item_name)).setText(contact.getName());
-			((TextView)item.findViewById(R.id.item_title)).setText(contact.getTitle());
-			((TextView)item.findViewById(R.id.item_phone)).setText(contact.getPhone());
-			
 			return item;
 		}
 	}
-    
-}
+ }
 
 
